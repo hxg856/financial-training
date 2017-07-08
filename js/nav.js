8c0dc8c856014291b7bf730924ea01e58ee14eea
hxg856 <452089277@qq.com>
2017-07-08 21:06:55 +0800

    addnav

--- js/nav.js
+++ js/nav.js
@@ -8,7 +8,7 @@ $(function () {
             //console.log(res);
             userID = res.data.user.userID;
             var userName = "res.data.user.usename";
-            var $name = $(" <p class='navbar-text navbar-left' style='font-size:1.2em'></p>")
+            var $name = $(" <p class='navbar-text navbar-left' style='font-size:1.2em'></p>");
             $name.text(userName);
             $("#afname").before($name);
         }
@@ -17,7 +17,7 @@ $(function () {
         alert("xxx");
         var data = {
             userID: userID
-        }
+        };
         $.ajax({
             url: "http://119.29.236.25/exit",
             type: "POST",
@@ -29,4 +29,4 @@ $(function () {
         })
     })
 
-})
+});
