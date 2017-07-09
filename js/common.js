d432620eed0c4246efcdea1d0cf7fa05f072b801
hxg856 <452089277@qq.com>
2017-07-09 22:46:25 +0800

    7-9

--- js/common.js
+++ js/common.js
@@ -47,6 +47,19 @@ $(function () {
                 console.log("error", res)
             }
         });
+        var data = {
+            companyName: "hxg"
+        };
+        $.ajax({
+            url: "http://119.29.236.25/search_report",
+            type: "POST",
+            dataType: "json",
+            data: data,
+            success: enterVerifyInfo,
+            error: function (res) {
+                console.log("error", res)
+            }
+        });
 
         function enterCompanyInfo(res) {
             var com = res.data.company;
@@ -57,9 +70,183 @@ $(function () {
         function enterloanInfo(res) {
             var loan = res.data.company;
             $("#cCredit").text(loan.LoanAim);
-            alert(loan.LoanAim);
+            switch (loan.RepaymentMethod) {
+                case 1:
+                    $("#cRepayMethod").text("一次性还清");
+                    break;
+                case 2:
+                    $("#cRepayMethod").text("等额本息分期还款");
+                    break;
+                case 3:
+                    $("#cRepayMethod").text("等额本金分期还款");
+                    break;
+                case 4:
+                    $("#cRepayMethod").text("活期还款");
+                    break;
+            }
+            $("#cRepayTime").text(loan.RepaymentTime);
+            if (loan.RepaymentMethod == 1) {
+                //一次性
+                if (loan.RepaymentTime <= 12) {
+                    $("#cGrede2").text(20);
+                }
+                if (loan.RepaymentTime <= 60 && loan.RepaymentTime > 12) {
+                    $("#cGrede2").text(15);
+                }
+                if (loan.RepaymentTime <= 120 && loan.RepaymentTime > 60) {
+                    $("#cGrede2").text(10);
+                }
+                if (loan.RepaymentTime > 120) {
+                    $("#cGrede2").text(5);
+                }
+            }
+            if (loan.RepaymentMethod == 2 || loan.RepaymentMethod == 3) {
+                //分期
+                if (loan.RepaymentTime <= 12) {
+                    $("#cGrede2").text(5);
+                }
+                if (loan.RepaymentTime <= 60 && loan.RepaymentTime > 12) {
+                    $("#cGrede2").text(10);
+                }
+                if (loan.RepaymentTime <= 120 && loan.RepaymentTime > 60) {
+                    $("#cGrede2").text(15);
+                }
+                if (loan.RepaymentTime > 120) {
+                    $("#cGrede2").text(20);
+                }
+            }
+            if (loan.RepaymentMethod == 4) { //活期
+                $("#cGrede2").text("0分");
+            }
+
+
+
+
+            switch (loan.LoanCredit) {
+                case 1:
+                    $("#cCharater").text("优秀/Excellent");
+                    $("#cGrede3").text(10)
+                    break;
+                case 2:
+                    $("#cCharater").text(5);
+                    break;
+                case 3:
+                    $("#cCharater").text(0);
+                    break;
+                case 4:
+                    $("#cCharater").text(-5);
+                    break;
+                case 5:
+                    $("#cCharater").text(-10);
+                    break;
+            }
+            switch (loan.CompanyAchievement.Pal) {
+                case 1:
+                    $("#cPal").text("盈利");
+                    break;
+                case 2:
+                    $("#cPal").text("亏损");
+                    break;
+            }
+            switch (loan.CompanyAchievement.Sum) {
+                case 1:
+                    $("#cSum").text(loan.CompanyAchievement.Sum + "万元");
+                    break;
+                case 2:
+                    $("#cSum").text(loan.CompanyAchievement.Sum + "万元");
+                    break;
+            }
+            if (loan.CompanyAchievement.Pal == 1) {
+                if (loan.CompanyAchievement.Sum <= 100) {
+
+                }
+                if (loan.CompanyAchievement.Sum > 100 && loan.CompanyAchievement.Sum <= 200) {
+
+                }
+                if (loan.CompanyAchievement.Sum > 100 && loan.CompanyAchievement.Sum <= 200) {
+
+                }
+                if (loan.CompanyAchievement.Sum > 200 && loan.CompanyAchievement.Sum <= 300) {
+
+                }
+                if (loan.CompanyAchievement.Sum > 300 && loan.CompanyAchievement.Sum <= 500) {
+
+                }
+                if (loan.CompanyAchievement.Sum > 500 && loan.CompanyAchievement.Sum <= 1000) {
+
+                }
+                if (loan.CompanyAchievement.Sum > 1000 && loan.CompanyAchievement.Sum <= 3000) {
+
+                }
+                if (loan.CompanyAchievement.Sum > 3000 && loan.CompanyAchievement.Sum <= 5000) {
+
+                }
+                if (loan.CompanyAchievement.Sum > 5000 && loan.CompanyAchievement.Sum <= 10000) {
+
+                }
+            }
+            if (loan.CompanyAchievement.Pal == 2) {
+
+            }
+            switch (loan.HistoricalCredit.LoanOrNot) {
+                case true:
+                    $("#cLoan").text("有信贷历史");
+                    break;
+                case false:
+                    $("#cLoan").text("信贷历史");
+                    break;
+            }
+            switch (loan.HistoricalCredit.RepayOrNot) {
+                case true:
+                    $("#cRepay").text("已还清");
+                    break;
+                case false:
+                    $("#cRepay").text("未还清");
+                    break;
+            }
 
         }
+
+        function enterVerifyInfo(res) {
+            var verify = res.data.report[0];
+            switch (verify.ExpectRange) {
+                case "F":
+                    $("#cGrede1").text("50");
+                    break;
+                case "E":
+                    $("#cGrede1").text("60");
+                    break;
+                case "D":
+                    $("#cGrede1").text("65");
+                    break;
+                case "C":
+                    $("#cGrede1").text("70");
+                    break;
+                case "B":
+                    $("#cGrede1").text("75");
+                    break;
+                case "A":
+                    $("#cGrede1").text("80");
+                    break;
+                case "S":
+                    $("#cGrede1").text("85");
+                    break;
+                case "SS":
+                    $("#cGrede1").text("90");
+                    break;
+                case "SSS":
+                    $("#cGrede1").text("95");
+                    break;
+            }
+            $("#cGrede7").text(verify.RealRange.Score);
+            if ($("#cGrede7").text() > $("#cGrede1").text()) {
+                $("#cDis").text("符合");
+            } else {
+                $("#cDis").text("不符合");
+            }
+
+        }
+
     })
 
 })
